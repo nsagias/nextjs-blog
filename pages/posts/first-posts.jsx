7e5147a3933279d61be1fdb4772937de68d8fef0
Nick <nsagias@gmail.com>
2023-03-12 17:07:30 -0400

    add Head component

--- pages/posts/first-posts.jsx
+++ pages/posts/first-posts.jsx
@@ -1,8 +1,12 @@
 import Link from "next/link";
+import Head from "next/head";
 
 export default function FirstPost() {
   return (
     <>
+      <Head>
+        <title>First Post Head</title>
+      </Head>
       <h1>First Post</h1>
       <h2><Link href="/">Home Page</Link></h2>
     </>
