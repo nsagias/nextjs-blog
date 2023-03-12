563d40b2abd0c8044135bd88bb6cc6fb1f0f933d
Nick <nsagias@gmail.com>
2023-03-12 19:14:19 -0400

    add section import utilStyles

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
-import Head from 'next/head';
-import styles from '../styles/Home.module.css';
-import Link from "next/link";
-import Layout, { siteTitle } from '../components/layout';
+import Head from "next/head";
+import Layout, { siteTitle } from "../components/layout";
+import utilStyles from "../styles/utils.module.css";
 
 export default function Home() {
   return (
@@ -9,6 +8,13 @@ export default function Home() {
       <Head>
         <title>{siteTitle}</title>
       </Head>
+      <section className={utilStyles.headingMd}>
+        <p>Starter blog</p>
+        <p>
+          (This is a sample website - you’ll be building a site like this on{' '}
+          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+        </p>
+      </section>
    </Layout>
   );
 }
