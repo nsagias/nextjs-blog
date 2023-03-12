380d90aac7421daacda9a309fa19daf067401605
Nick <nsagias@gmail.com>
2023-03-12 17:11:46 -0400

    add Script component and lazyload fb sdk

--- pages/posts/first-posts.jsx
+++ pages/posts/first-posts.jsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import Head from "next/head";
+import Script from "next/script";
 
 export default function FirstPost() {
   return (
@@ -7,6 +8,12 @@ export default function FirstPost() {
       <Head>
         <title>First Post Head</title>
       </Head>
+      <Script 
+        src="https://connect.facebook.net/en_US/sdk.js"
+        strategy="lazyOnLoad"
+        onLoad={()=> console.log(`Scrip loaded correcly. windowFB loaded`)}
+      
+      />
       <h1>First Post</h1>
       <h2><Link href="/">Home Page</Link></h2>
     </>
