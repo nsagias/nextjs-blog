0fd9fb08d0382b25a3f840a6ecf34b044c07cd4a
Nick <nsagias@gmail.com>
2023-03-12 19:08:45 -0400

    close Link jsx

--- components/layout.jsx
+++ components/layout.jsx
@@ -32,39 +32,45 @@ export default function Layout({ children, home }) {
         content="summary_large_image" 
       />
 
-    <header className={styles.header}>
-      {home ? (
-        <>
-          <Image 
-            priority
-            src="/images/profile_next_tutorial.jpg"
-            className={utilStyles.borderCircle}
-            height={144}
-            width={144}
-            alt=""
-          />
-          <h1 className={utilStyles.heading2Xl}>{name}</h1>
-        </>
-      ) : (
-        <>
-          <Link href="/">
+      <header className={styles.header}>
+        {home ? (
+          <>
             <Image 
               priority
               src="/images/profile_next_tutorial.jpg"
               className={utilStyles.borderCircle}
-              height={108}
-              width={108}
+              height={144}
+              width={144}
               alt=""
             />
-            <h2 className={utilStyles.headingLg}>
-                <Link href="/" className={utilStyles.colorInherit}>
-                  {name}
-                </Link>
-            </h2>
-          </Link>
-        </>
+            <h1 className={utilStyles.heading2Xl}>{name}</h1>
+          </>
+        ) : (
+          <>
+            <Link href="/">
+              <Image 
+                priority
+                src="/images/profile_next_tutorial.jpg"
+                className={utilStyles.borderCircle}
+                height={108}
+                width={108}
+                alt=""
+              />
+            </Link>
+              <h2 className={utilStyles.headingLg}>
+                  <Link href="/" className={utilStyles.colorInherit}>
+                    {name}
+                  </Link>
+              </h2>
+          </>
+        )}
+      </header>
+      <main>{children}</main>
+      {!home && (
+        <div className={styles.backToHome}> 
+          <Link href="/">{`Back to home`}</Link>
+        </div>
       )}
-    </header>
     </div>
   );
 }
