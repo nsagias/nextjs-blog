58064ed551c379119d3cc0583cff9e06e2c85765
Nick <nsagias@gmail.com>
2023-03-12 18:41:33 -0400

    refactor layout impots

--- components/layout.jsx
+++ components/layout.jsx
@@ -1,5 +1,17 @@
+import Head from "next/head";
+import Image from "next/image";
+import Link from "next/link";
+import utilStyles from "../styles/utils.module.css";
 import styles from "./layout.module.css";
 
-export default function Layout({children}) {
-  return <div className={styles.container}>{children}</div>;
+const name = "nick"
+export const siteTitle = "Nextjs Sample Website";
+
+export default function Layout({ children, home }) {
+  return (
+
+    <div className={styles.container}>
+
+    </div>
+  );
 }
