482f4304be4d974dc8525409a1ba05dc5eaef624
Nick <nsagias@gmail.com>
2023-03-12 20:21:44 -0400

    rename library to posts

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,11 @@
 import Head from "next/head";
 import Layout, { siteTitle } from "../components/layout";
 import utilStyles from "../styles/utils.module.css";
+import {} from "../lib/posts";
+
+
+
+
 
 export default function Home() {
   return (
