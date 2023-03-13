c5fb73a4d79b6744eebff835768dafed5e64d056
Nick <nsagias@gmail.com>
2023-03-12 20:32:50 -0400

    render allpostdata

--- pages/index.jsx
+++ pages/index.jsx
@@ -10,20 +10,27 @@ export async function getStaticProps() {
   };
 }
 
-
-
 export default function Home() {
   return (
    <Layout>
       <Head>
         <title>{siteTitle}</title>
       </Head>
-      <section className={utilStyles.headingMd}>
-        <p>Starter blog</p>
-        <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Starter blog</h2>
+        <ul className={utilStyles.list}>
+          {Array.isArray(allPostsData) && 
+          allPostsData.length > 0 && 
+          allPostsData.map(({id, data, title}) => {
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {data}
+            </li>
+          })}
+        </ul>
       </section>
    </Layout>
   );
