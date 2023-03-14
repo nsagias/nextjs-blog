8c60675c166b5ebf469c54da9ac858a9a3950669
Nick <nsagias@gmail.com>
2023-03-13 20:26:52 -0400

    fix import of utils modules

--- components/Layout/layout.jsx
+++ components/Layout/layout.jsx
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
-import utilStyles from "../styles/utils.module.css";
+import utilStyles from "../../styles/utils.module.css";
 import styles from "./layout.module.css";
 
 const name = "nick"
