8a2324ace7df4a8aa66629b4e5a637c8571eb9ec
Nick <nsagias@gmail.com>
2023-03-13 21:56:52 -0400

    update Post to render data

--- pages/posts/[id].jsx
+++ pages/posts/[id].jsx
@@ -22,6 +22,16 @@ export async function getStaticProps({ params }) {
   };
 }
 
-export default function Post() {
-  return <Layout>...</Layout>;
+export default function Post({ postData }) {
+  return (
+    <Layout>
+      {postData.title}
+      <br />
+      {postData.id}
+      <br />
+      {postData.date}
+      <br />
+      <div dangerouslySetInnerHTML={{  __: postData.contentHTML}} />
+    </Layout>
+  );
 }
