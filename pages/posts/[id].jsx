16b4d71dcfc213acb243815a072c86d029a24a5a
Nick <nsagias@gmail.com>
2023-03-13 21:01:47 -0400

    add getStaticProps to id.jsx

--- pages/posts/[id].jsx
+++ pages/posts/[id].jsx
@@ -1,11 +1,24 @@
 import Layout from '../../components/layout';
 import { getAllPostIds } from "../../Services/posts";
+import { getPostData } from '../../Services/postsServices';
 
 export async function getStaticPaths() {
   const path = getAllPostIds();
   return {
     paths,
     fallback: false,
+  };
+}
+
+export async function getStaticProps({ params }) {
+  // get post data
+  const postData = await getPostData(params.id);
+
+  // return opbject as props : {};
+  return {
+    props: {
+      postData,
+    },
   };
 }
 
